fe755656bb0521c03fa4677e3773ce0c9516d316
Jason Humphreys <jasonphumphreys@gmail.com>
2016-12-31 11:08:37 -0500

    PacMan movement fixed

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ class Character {
     this.movingLeft = false;
     this.movingUp = false;
     this.movingDown = false;
-    this.direction = 'none';
   }
   assignRow () {
     if (this.topPos % 26 !== 0)
@@ -34,6 +33,7 @@ class Character {
 class PacMan extends Character {
   constructor(speed, leftPos, topPos, canBeEaten) {
     super(speed, leftPos, topPos, canBeEaten)
+    this.lastKey = 'none';
   }
 }
 
@@ -78,15 +78,19 @@ function initiateEventListeners () {
 }
 function move(e) {
   if (e.keyCode === 39 && pacMan.movingRight !== true) {
+    pacMan.lastKey = 'right';
     moveRight();
   }
   else if (e.keyCode === 38 && pacMan.movingUp !== true) {
+    pacMan.lastKey = 'up';
     moveUp();
   }
   else if (e.keyCode === 37 && pacMan.movingLeft !== true) {
+    pacMan.lastKey = 'left';
     moveLeft();
   }
   else if (e.keyCode === 40 && pacMan.movingDown !== true) {
+    pacMan.lastKey = 'down';
     moveDown();
   }
 }
@@ -103,6 +107,10 @@ function moveRight () {
       clearInterval(animate);
     // else if (pacManHit() === true)
     //   clearInterval(animate);
+    else if (changeDirectionUp() === true || changeDirectionDown() === true) {
+      pacMan.movingRight = false;
+      clearInterval(animate);
+    }
     else if (horizontalGrid(pacMan) === true && movingRightRowDetection(pacMan) === false) {
       if (pacMan.movingLeft === true){
         pacMan.movingRight = false;
@@ -128,6 +136,10 @@ function moveDown () {
       clearInterval(animate);
     // else if (pacManHit() === true)
     //   clearInterval(animate);
+    else if (changeDirectionRight() === true || changeDirectionLeft() === true) {
+      pacMan.movingDown = false;
+      clearInterval(animate);
+    }
     else if (verticalGrid(pacMan) === true && movingDownColumnDetection(pacMan) === false) {
       if (pacMan.movingUp === true) {
         pacMan.movingDown = false;
@@ -153,6 +165,10 @@ function moveLeft () {
       clearInterval(animate);
     // else if (pacManHit() === true)
     //   clearInterval(animate);
+    else if (changeDirectionUp() === true || changeDirectionDown() === true) {
+      pacMan.movingLeft = false;
+      clearInterval(animate);
+    }
     else if (horizontalGrid(pacMan) === true && movingLeftRowDetection(pacMan) === false) {
       if (pacMan.leftPos <= 0 || pacMan.movingRight === true) {
         pacMan.movingLeft = false;
@@ -178,6 +194,10 @@ function moveUp () {
       clearInterval(animate);
     // else if (pacManHit() === true)
     //   clearInterval(animate);
+    else if (changeDirectionRight() === true || changeDirectionLeft() === true) {
+      pacMan.movingUp = false;
+      clearInterval(animate);
+    }
     else if (verticalGrid(pacMan) === true && movingUpColumnDetection(pacMan) === false) {
       if (pacMan.topPos <= 0 || pacMan.movingDown === true) {
         pacMan.movingUp = false;
@@ -194,7 +214,32 @@ function moveUp () {
     }
   }
 }
-
+//These change direction functions will halt an animation interval from continuing once pacMan
+//is no longer moving in that direction
+function changeDirectionUp() {
+  if (pacMan.lastKey === 'up' && horizontalGrid(pacMan) === true && verticalGrid(pacMan) === true
+      && movingUpColumnDetection(pacMan) === false) {
+    return true
+  }
+}
+function changeDirectionDown() {
+  if (pacMan.lastKey === 'down' && horizontalGrid(pacMan) === true && verticalGrid(pacMan) === true
+      && movingDownColumnDetection(pacMan) === false) {
+    return true
+  }
+}
+function changeDirectionLeft() {
+  if (pacMan.lastKey === 'left' && horizontalGrid(pacMan) === true && verticalGrid(pacMan) === true
+      && movingLeftRowDetection(pacMan) === false) {
+    return true
+  }
+}
+function changeDirectionRight() {
+  if (pacMan.lastKey === 'right' && horizontalGrid(pacMan) === true && verticalGrid(pacMan) === true
+      && movingRightRowDetection(pacMan) === false) {
+    return true
+  }
+}
 //PacMan can only turn when he is on the grid
 
 function horizontalGrid (char) {
